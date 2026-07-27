5885b1bb8bc59c47e8c9ece89b785fcc2a249147
charan192311001 <reddikumarcharantej1001.sse@saveetha.com>
2026-07-27 10:12:02 +0530

    fix: Upgrade AGP to 8.11.1 and Kotlin to 2.2.20 and disable release lint check

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,11 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    lint {
+        checkReleaseBuilds = false
+        isAbortOnError = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
